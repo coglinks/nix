ad46ad0497ad2bf7a7a1bc7f1f6d19b94f1ce1b2
incog267 <incog267@gmail.com>
2025-04-09 23:05:24 +0300

    cleanup

--- modules/global/fixes.nix
+++ modules/global/fixes.nix
@@ -4,5 +4,7 @@
   # Prevents suspend of mouse after a few seconds
   # You might be able to use `usbcore.autosuspend=-1` to disable entirely
   # ref = https://discourse.nixos.org/t/turn-off-autosuspend-for-usb/58933
-  boot.kernelParams = [ "usbcore.autosuspend=580" ]; # if autosuspend=120 => wait for 2 minutes. 
+  boot.kernelParams = [ 
+    "usbcore.autosuspend=120" 
+  ]; # if autosuspend=120 => wait for 2 minutes. 
 }

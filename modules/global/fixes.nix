cb83c659d7a733cd8d3db8e097a8a47830c667e4
incog267 <incog267@gmail.com>
2025-04-14 21:31:34 +0300

    adjusted mouse sleep issue

--- modules/global/fixes.nix
+++ modules/global/fixes.nix
@@ -5,6 +5,6 @@
   # You might be able to use `usbcore.autosuspend=-1` to disable entirely
   # ref = https://discourse.nixos.org/t/turn-off-autosuspend-for-usb/58933
   boot.kernelParams = [ 
-    "usbcore.autosuspend=120" 
+    "usbcore.autosuspend=1000" 
   ]; # if autosuspend=120 => wait for 2 minutes. 
 }

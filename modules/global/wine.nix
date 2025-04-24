c4522ba796372d9f1697cf9447e85a224b3f1f46
incogshift <incog267@gmail.com>
2025-04-24 05:36:24 +0300

    rm pkgs

--- modules/global/wine.nix
+++ modules/global/wine.nix
@@ -4,17 +4,6 @@
   environment.systemPackages = with pkgs; [
     # support both 32-bit and 64-bit applications
     wineWowPackages.stable
-    # support 32-bit only
-    wine
-    # support 64-bit only
-    (wine.override { wineBuild = "wine64"; })
-    wine64
-    # wine-staging (version with experimental features)
-    wineWowPackages.staging
-    # winetricks (all versions)
     winetricks
-    # native wayland support (unstable)
-    wineWowPackages.waylandFull
-
   ];
 }

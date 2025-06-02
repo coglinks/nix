5e47e6369252dccc949aad917b621a87c619e6b0
incogshift <incog267@gmail.com>
2025-06-02 13:47:47 +0300

    nixos: enabled autoupgrades, updated state version

--- modules/global/nix.nix
+++ modules/global/nix.nix
@@ -8,6 +8,8 @@
   #  inherit (config.nixpkgs) config;
   #};
 
+  system.autoUpgrade.enable = true;
+
   home-manager.backupFileExtension = "hm-backup";
 
   nix.settings = {
@@ -32,5 +34,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 }

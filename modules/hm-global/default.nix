36095672902ea29839a179a1462c19d526045192
incogshift <incog267@gmail.com>
2025-06-10 10:09:51 +0300

    nixos: added nix default apps options

---
+++ modules/hm-global/default.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+	xdg.mimeApps = {
+		enable = true;
+		defaultApplications = {
+			"text/html" = "librewolf.desktop";
+			"x-scheme-handler/http" = "librewolf.desktop";
+			"x-scheme-handler/https" = "librewolf.desktop";
+			"x-scheme-handler/about" = "librewolf.desktop";
+			"x-scheme-handler/unknown" = "librewolf.desktop";
+		};
+	};
+}

f395e296379f0968197d0fe4323da5a93dce83bf
incogshift <incog267@gmail.com>
2025-06-13 18:55:37 +0300

    nix: cleanup up git config

--- modules/global/home-manager/git.nix
+++ modules/global/home-manager/git.nix
@@ -12,10 +12,9 @@
     lfs.enable = true;
   };
   home.packages = with pkgs; [
-    git # cli #vcs #git-tools
-    git-lfs
     git-crypt # cli #encryption #git-tools
     lazygit # tui #vcs #git-tools
     git-remote-gcrypt # cli #encryption-plugin-for-git #git-tools
+    git-credential-manager
   ];
 }

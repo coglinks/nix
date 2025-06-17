364496fad72b202c1c7b1017a59f0587add32453
incogshift <incog267@gmail.com>
2025-06-17 10:17:26 +0300

    hyprland: updated screenshot config

--- non-nix/dotfiles/.config/hypr/scripts/screenshot.sh
+++ non-nix/dotfiles/.config/hypr/scripts/screenshot.sh
@@ -2,10 +2,11 @@
 
 tmpimg=$(mktemp --suffix=.png)
 
-grim -g "$(slurp)" "$tmpimg" && {
-    wl-copy < "$tmpimg"
-    xclip -selection clipboard -t image/png -i "$tmpimg"
-    file "$tmpimg"
-    rm "$tmpimg"
-}
-
+wayfreeze \
+  --after-freeze-cmd "
+    grim -g \"\$(slurp)\" \"$tmpimg\" &&
+    wl-copy < \"$tmpimg\" &&
+    xclip -selection clipboard -t image/png -i \"$tmpimg\" &&
+    file \"$tmpimg\" &&
+    rm \"$tmpimg\"; killall wayfreeze
+  "

eef855154a75b3816469fe0ab7c547d7f509c9dd
incogshift <incog267@gmail.com>
2025-06-17 10:18:30 +0300

    nix: nvim config link add

--- home.nix
+++ home.nix
@@ -18,6 +18,7 @@
     ./modules/global/home-manager/git.nix
     ./modules/global/home-manager/hyprland.nix
     ./modules/global/home-manager/pkgs-essential.nix
+    ./modules/global/home-manager/nvim.nix
     ./modules/loq/hm-pkgs.nix
 #    ./non-nix/symlink.nix
     ./non-nix/programs/desktop-entries.nix

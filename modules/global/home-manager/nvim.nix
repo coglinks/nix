2dce9642b302fdc8e963437d45d87ab1886c9711
incogshift <incog267@gmail.com>
2025-06-23 18:43:35 +0300

    nvim: image.nvim fix

--- modules/global/home-manager/nvim.nix
+++ modules/global/home-manager/nvim.nix
@@ -5,36 +5,37 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-		viAlias = true;
-		plugins = [
-				{
-					plugin = pkgs.vimPlugins.sqlite-lua;
-					config = "let g:sqlite_clib_path = '${pkgs.sqlite.out}/lib/libsqlite3${pkgs.stdenv.hostPlatform.extensions.sharedLibrary}'";
-				}
-				{
-					plugin = pkgs.vimPlugins.blink-cmp;
-				}
-		];
-		extraLuaConfig = ''
-		dofile(vim.fn.stdpath("config") .. "/user-init.lua")
-		'';
-	};
-	home.packages = with pkgs; [
-		mercurialFull
-		lua-language-server
-		quarto
+    viAlias = true;
+    plugins = [
+        pkgs.vimPlugins.image-nvim
+        {
+          plugin = pkgs.vimPlugins.sqlite-lua;
+          config = "let g:sqlite_clib_path = '${pkgs.sqlite.out}/lib/libsqlite3${pkgs.stdenv.hostPlatform.extensions.sharedLibrary}'";
+        }
+        {
+          plugin = pkgs.vimPlugins.blink-cmp;
+        }
+    ];
+    extraLuaConfig = ''
+    dofile(vim.fn.stdpath("config") .. "/user-init.lua")
+    '';
+  };
+  home.packages = with pkgs; [
+    mercurialFull
+    lua-language-server
+    quarto
     rocmPackages.llvm.clang # cli #compiler
-		tree-sitter
-		aider-chat
-		sqlite
-		libgcc
+    tree-sitter
+    aider-chat
+    sqlite
+    libgcc
     libgcc #nvim dependancy
-		deno
-		marksman
+    deno
+    marksman
     nodejs
-		python313Packages.pylatexenc
-		luajitPackages.jsregexp
+    python313Packages.pylatexenc
+    luajitPackages.jsregexp
     lua5_1 # cli #programming-language
     luarocks # cli #package-manager
-	];
+  ];
 }

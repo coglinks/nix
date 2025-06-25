b0eb383b1a8637ce0da4cf7b90992efe3d5b6cd6
incogshift <incog267@gmail.com>
2025-06-25 23:19:12 +0300

    nix: nh; update to preference

--- modules/global/nix.nix
+++ modules/global/nix.nix
@@ -12,6 +12,13 @@
 
   home-manager.backupFileExtension = "hm-backup";
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 30d --keep 20";
+    flake = "/home/incogshift/st/nix#loq";
+  };
+
   nix.settings = {
     experimental-features = [
       "nix-command"

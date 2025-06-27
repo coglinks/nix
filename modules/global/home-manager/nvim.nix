110ca04e76085343afe0f6ae2d158467c8c5c6bc
incogshift <incog267@gmail.com>
2025-06-27 12:40:03 +0300

    nvim: added nix plugins

--- modules/global/home-manager/nvim.nix
+++ modules/global/home-manager/nvim.nix
@@ -11,6 +11,16 @@
     plugins = [
       pkgs.vimPlugins.image-nvim
       pkgs.vimPlugins.markview-nvim
+      pkgs.vimPlugins.quarto-nvim
+      pkgs.vimPlugins.blink-cmp
+      pkgs.vimPlugins.jupytext-nvim
+      pkgs.vimPlugins.mason-nvim
+      pkgs.vimPlugins.molten-nvim
+      pkgs.vimPlugins.snacks-nvim
+      pkgs.vimPlugins.telescope-nvim
+      pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+      pkgs.vimPlugins.fzf-lua
+      pkgs.vimPlugins.outline-nvim
       {
         plugin = pkgs.vimPlugins.sqlite-lua;
         config = "let g:sqlite_clib_path = '${pkgs.sqlite.out}/lib/libsqlite3${pkgs.stdenv.hostPlatform.extensions.sharedLibrary}'";
@@ -29,11 +39,11 @@
     quarto
     rocmPackages.llvm.clang # cli #compiler
     tree-sitter
+    ripgrep
     aider-chat
     sqlite
     libgcc
-    libgcc #nvim dependancy
-    deno
+    libgcc    deno
     marksman
     nodejs
     python313Packages.pylatexenc

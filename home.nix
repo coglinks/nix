a0d55fb3583fa99426c26fa6d901159bd391448c
coglinks <fairoosmohd2@gmail.com>
2025-07-26 23:05:43 +0300

    nix: moved home-manager settings to nix.nix

--- home.nix
+++ home.nix
@@ -30,16 +30,4 @@
     LANG = "en_US.UTF-8";
     LC_ALL = "en_US.UTF-8";
   };
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }

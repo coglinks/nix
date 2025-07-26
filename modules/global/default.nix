c0a802f7d8f3c80b330c5b5eaf091575d098da21
coglinks <fairoosmohd2@gmail.com>
2025-07-26 22:38:40 +0300

    nix: imports file for modules/global

---
+++ modules/global/default.nix
@@ -0,0 +1,19 @@
+{ ... }:
+
+{
+  imports = [
+    ./desktop-env
+    ./hardware
+    ./home-manager
+    ./styling
+    ./boot.nix
+    ./container.nix
+    ./gaming.nix
+    ./lanzaboote.nix
+    ./nix.nix
+    ./ollama.nix
+    ./pkgs.nix
+    ./security.nix
+    ./user.nix
+  ];
+}

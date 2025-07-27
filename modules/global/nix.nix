840b26e5f7ec53fbb28f5694ada0f95b0b5b2d33
coglinks <fairoosmohd2@gmail.com>
2025-07-27 13:40:17 +0300

    nix: added trusted-substitutors

--- modules/global/nix.nix
+++ modules/global/nix.nix
@@ -27,6 +27,10 @@
     substituters = [
       "https://nix-community.cachix.org"
     ];
+    trusted-substituters = [
+      "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+    ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];

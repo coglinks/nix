fa32c2f7502acaa58f70783fc3815226180ef16c
coglinks <fairoosmohd2@gmail.com>
2025-08-17 16:23:29 +0300

    nix: rm flox

--- flake.nix
+++ flake.nix
@@ -14,12 +14,10 @@
     trusted-substituters = [
       "https://cache.nixos.org"
       "https://hyprland.cachix.org"
-      "https://cache.flox.dev"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
     ];
     extra-substituters = [
       "https://yazi.cachix.org"

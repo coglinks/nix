d607eee6e4e2d1116cc2504bc91f6d2799c70955
coglinks <fairoosmohd2@gmail.com>
2025-08-17 20:34:56 +0200

    zsh: configured mkOrder & added yazi integration

--- modules/home/yazi.nix
+++ modules/home/yazi.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.yazi = {
@@ -6,4 +6,16 @@
     enableZshIntegration = true;
     package = pkgs.yazi;
   };
+  # zsh shell wrapper: change the current working directory when exiting Yazi
+  programs.zsh.initContent = lib.mkOrder 1000
+    # bash
+    ''
+      function y() {
+        local tmp="''$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        yazi "''$@" --cwd-file="''$tmp"
+        IFS= read -r -d ''' cwd < "''$tmp"
+        [ -n "''$cwd" ] && [ "''$cwd" != "''$PWD" ] && builtin cd -- "''$cwd"
+        rm -f -- "$tmp"
+      }
+    '';
 }

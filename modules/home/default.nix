cfb06b1b0e218d3d03a92550c2bca40b437758bd
coglinks <fairoosmohd2@gmail.com>
2025-08-17 21:09:20 +0200

    rm non-nixCats nvim

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -21,7 +21,6 @@
     ./micro.nix                       # nano replacement
     ./nemo.nix                        # file manager
     ./nix-search/nix-search.nix       # TUI to search nixpkgs
-    ./nvim.nix                        # neovim editor
     ./obsidian.nix
     ./p10k/p10k.nix
     ./packages                        # other packages
